6e4d1ac26054393c60b6e2afa2efd1f9bfc61b00
Cai_Niao <cai_niao@Cai-Niaos-iMac.local>
2018-03-01 19:56:19 -0800

    add zsh_config

--- my_config.sh
+++ my_config.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -vx
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+# config zsh
+# ref: https://www.jianshu.com/p/7de00c73a2bb
+
+# git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.zshrc ~/.zshrc.orig
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+cp ./zsh_config/oh-my-zsh//templates/zshrc.zsh-template ~/.zshrc
 chsh -s /bin/zsh
 alias ls='ls -G'
 alias ll='ls -l'
