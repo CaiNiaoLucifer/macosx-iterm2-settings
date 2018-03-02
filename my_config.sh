992a9e79807fc9c5fc6de9092e1e094374fe5a40
Cai_Niao <cai_niao@Cai-Niaos-iMac.local>
2018-03-02 13:29:16 +0800

    update theme

--- my_config.sh
+++ my_config.sh
@@ -3,8 +3,14 @@
 # ref: https://www.jianshu.com/p/7de00c73a2bb
 
 # git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+# https://github.com/fcamblor/oh-my-zsh-agnoster-fcamblor
+# https://github.com/powerline/fonts
+
 cp ~/.zshrc ~/.zshrc.orig
-cp ./zsh_config/oh-my-zsh//templates/zshrc.zsh-template ~/.zshrc
+cp ./zsh_config/oh-my-zsh//templates/my_zsh.zshrc ~/.zshrc
+
+./source/oh-my-zsh-agnoster-fcamblor/install
+
 chsh -s /bin/zsh
 alias ls='ls -G'
 alias ll='ls -l'
