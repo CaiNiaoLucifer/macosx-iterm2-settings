9751b1d2f3d52755436b86cef848a9482db80ee9
duyongwei <duyongwei@xiaomi.com>
2018-03-02 11:28:40 +0800

    add myconfig

---
+++ my_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -vx
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.zshrc ~/.zshrc.orig
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s /bin/zsh
+alias ls='ls -G'
+alias ll='ls -l'
+alias grep='grep --color'
+
